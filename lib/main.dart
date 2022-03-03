68103e8aea2b90bfe586ddbcb63cc97208744f7a
fukuda <kfukuda@student.42tokyo.jp>
2022-03-03 20:14:03 +0900

    初級編2:画面の更新(statefulWidget/setState)
    - 変更反映時はsetStateを使う
    - stateless→状態を持たない
    - stateful→状態を持つ

--- lib/main.dart
+++ lib/main.dart
@@ -31,16 +31,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,15 +39,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String _type = "偶数";
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+      if (_counter % 2 == 0) {
+        _type = "偶数";
+      } else {
+        _type = "奇数";
+      }
     });
   }
 
@@ -65,46 +56,31 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          children: [
-            Icon(Icons.create),
-            Text("初めてのタイトル"),
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            if (_counter % 2 == 0)
+              Text('偶数です', style: TextStyle(fontSize: 20, color: Colors.red)),
+            if (_counter % 2 == 1)
+              Text('奇数です', style: TextStyle(fontSize: 20, color: Colors.red))
           ],
         ),
       ),
-      drawer: Drawer(child: Center(child: Text("Drawer"))),
-      body: Column(
-        children: [
-          Text("初めてのテキスト"),
-          Text("2番目のテキスト"),
-          TextButton(
-            onPressed: () => {print("ボタンがおされたよ")},
-            child: Text("更新"),
-          ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Icon(
-              Icons.favorite,
-              color: Colors.pink,
-              size: 24.0,
-            ),
-            Icon(
-              Icons.audiotrack,
-              color: Colors.green,
-              size: 30.0,
-            ),
-            Icon(
-              Icons.beach_access,
-              color: Colors.blue,
-              size: 36.0,
-            ),
-          ],
-        ),
-      ]),
       floatingActionButton: FloatingActionButton(
-          onPressed: () => {print("押したね？")},
-          child: Icon(Icons.timer)),
-      ); // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
