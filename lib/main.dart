535fffb8b8d20ff1924a06dc2929f515667568ee
k2491p <fukuda.dev10@gmail.com>
2022-03-06 09:02:12 +0900

    中級編1:非同期処理
    - Futureで呼び出したら処理の完了を待たずに
    - thenは処理が帰ってくるが順番の保証ができない
    - 多言語と同じように async-awaitで非同期処理完了まで待つこと可能

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,61 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-
+import 'package:flutter_practice/Async.dart';
 void main() {
-  runApp(MyApp());
+ runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     title: 'Flutter Demo',
+     theme: ThemeData(
+       primarySwatch: Colors.blue,
+     ),
+     home: MyHomePage(title: 'Flutter Demo Home Page'),
+   );
+ }
 }
-
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-  final String? title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+ MyHomePage({Key? key, this.title}) : super(key: key);
+ final String? title;
+ @override
+ _MyHomePageState createState() => _MyHomePageState();
 }
-
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  late AnimationController _animationControler;
-  late Animation _animation;
-
-  _play() async {
-    setState(() {
-      _animationControler.forward();
-    });
-  }
-
-  _stop() async {
-    setState(() {
-      _animationControler.stop();
-    });
-  }
-
-  _reverse() async {
-    setState(() {
-      _animationControler.reverse();
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _animationControler =
-        AnimationController(vsync: this, duration: Duration(seconds: 1));
-    _animation = _animationControler.drive(Tween(begin: 0.0, end: 2.0 * pi));
-  }
-
-  @override
-  void dispose() {
-    _animationControler.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
-      ),
-      body: Center(
-        child: AnimatedBuilder(
-          animation: _animation,
-          builder: (context, _) {
-            return Transform.rotate(
-                angle: _animation.value, child: Icon(Icons.cached, size: 100));
-          },
-        ),
-      ),
-      floatingActionButton:
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        FloatingActionButton(
-            onPressed: _play, child: Icon(Icons.arrow_forward)),
-        FloatingActionButton(onPressed: _stop, child: Icon(Icons.pause)),
-        FloatingActionButton(
-            onPressed: _reverse, child: Icon(Icons.arrow_back)),
-      ]),
-    );
-  }
+class _MyHomePageState extends State<MyHomePage> {
+ int _counter = 0;
+ void _incrementCounter() {
+   setState(() {
+     _counter++;
+   });
+    Async().asynctest4(); // 2-4も同様にここで呼び出す
+ }
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: Text(widget.title!),
+     ),
+     body: Center(
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: <Widget>[
+           Text(
+             'You have pushed the button this many times:',
+           ),
+           Text(
+             '$_counter',
+             key: Key('counter'),
+             style: Theme.of(context).textTheme.headline4,
+           ),
+         ],
+       ),
+     ),
+     floatingActionButton: FloatingActionButton(
+       key: Key('increment'),
+       onPressed: _incrementCounter,
+       tooltip: 'Increment',
+       child: Icon(Icons.add),
+     ),
+   );
+ }
 }
