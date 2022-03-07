ff49befb6f1ea52d4f38e1bec6be03521b77b73e
k2491p <fukuda.dev10@gmail.com>
2022-03-07 20:36:10 +0900

    中級編4:アプリのライフサイクル
    - AppLifecycleState.inactive:非アクティブ時
    - AppLifecycleState.paused:停止時
    - AppLifecycleState.resumed:再開時
    - AppLifecycleState.detached:破棄時

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,87 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/Async.dart';
-import 'package:flutter_practice/NullSample.dart';
-import 'package:flutter_practice/dummy.dart';
 void main() {
- runApp(MyApp());
+  runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     title: 'Flutter Demo',
-     theme: ThemeData(
-       primarySwatch: Colors.blue,
-     ),
-     home: MyHomePage(title: 'Flutter Demo Home Page'),
-   );
- }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
 }
 class MyHomePage extends StatefulWidget {
- MyHomePage({Key? key, this.title}) : super(key: key);
- final String? title;
- @override
- _MyHomePageState createState() => _MyHomePageState();
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
-class _MyHomePageState extends State<MyHomePage> {
- int _counter = 0;
- void _incrementCounter() {
-   setState(() {
-     _counter++;
-     print("call setState");
-   });
-    // Async().asynctest4(); // 2-4も同様にここで呼び出す
-    // NullSample.method1();
-    nextpage();
- }
-
-// ダミーで画面遷移を行う
-  void nextpage() async {
-    {
-      // ダミー画面へ遷移
-      await Navigator.of(context)
-          .pushReplacement(MaterialPageRoute(builder: (context) {
-        return DummyPage();
-      }));
-    }
-  }
-
-   @override
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+  @override
   void initState() {
-    print("call initState");
     super.initState();
-  }
-  @override
-  void didChangeDependencies() {
-    print("call didChangeDependencies");
-    super.didChangeDependencies();
-  }
-
- @override
- Widget build(BuildContext context) {
-   print("call build");
-   return Scaffold(
-     appBar: AppBar(
-       title: Text(widget.title!),
-     ),
-     body: Center(
-       child: Column(
-         mainAxisAlignment: MainAxisAlignment.center,
-         children: <Widget>[
-           Text(
-             'You have pushed the button this many times:',
-           ),
-           Text(
-             '$_counter',
-             key: Key('counter'),
-             style: Theme.of(context).textTheme.headline4,
-           ),
-         ],
-       ),
-     ),
-     floatingActionButton: FloatingActionButton(
-       key: Key('increment'),
-       onPressed: _incrementCounter,
-       tooltip: 'Increment',
-       child: Icon(Icons.add),
-     ),
-   );
- }
-
-
- @override
-  void didUpdateWidget(oldWidget) {
-    print("call didUpdateWidget");
-    super.didUpdateWidget(oldWidget);
-  }
-  @override
-  void deactivate() {
-    print("call deactivate");
-    super.deactivate();
+    WidgetsBinding.instance!.addObserver(this);
   }
   @override
   void dispose() {
-    print("call dispose");
+    print("dispose");
+    WidgetsBinding.instance!.removeObserver(this);
     super.dispose();
   }
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print("stete = $state");
+    switch (state) {
+      case AppLifecycleState.inactive:
+        print('非アクティブになったときの処理');
+        break;
+      case AppLifecycleState.paused:
+        print('停止されたときの処理');
+        break;
+      case AppLifecycleState.resumed:
+        print('再開されたときの処理');
+        break;
+      case AppLifecycleState.detached:
+        print('破棄されたときの処理');
+        break;
+    }
   }
+  int _counter = 0;
+  _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
